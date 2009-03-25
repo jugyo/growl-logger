859a929f310aeae5476a9aca30187819da344201
jugyo <kkohno@gmail.com>
2009-03-25 15:03:23 +0900

    format

--- examples/example.rb
+++ examples/example.rb
@@ -4,8 +4,8 @@ require File.dirname(__FILE__) + '/../lib/growl-logger'
 
 log = GrowlLogger.new :level => Logger::DEBUG, :growlnotify => true
 
-log.debug('debug test')
-log.info('info test')
-log.warn('warn test')
-log.error('error test')
-log.fatal('fatal test')
+log.debug('debug')
+log.info('info')
+log.warn('warn')
+log.error('error')
+log.fatal('fatal')
